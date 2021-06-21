999050217818ad58ad47d552d759547dd0deb4c6
TomaszBeben <tbeben@st.swps.edu.pl>
2021-06-21 18:59:03 +0200

    backend deployment v3

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ app.get('/', (req, res)=>{
     res.send('welcome to my API')
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = 5000
 
 mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
+    .then(() => app.listen(process.env.PORT || PORT, () => console.log(`Server is running on port ${PORT}`)))
     .catch((error) => console.log(error.message))
 
 mongoose.set('useFindAndModify', false)
