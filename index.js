dc56ad06e3ae1950b2958cb129b1d9bb62a11bf2
TomaszBeben <tbeben@st.swps.edu.pl>
2021-06-24 23:23:12 +0200

    make it better2

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ app.use(bodyParser.json({limit: '30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
 
-app.use('/posts', postRoutes)
-
 app.get('/', (req, res)=>{
     res.send('welcome to my API')
 })
 
-const PORT = 5000
+app.use('/posts', postRoutes)
+
+const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(process.env.PORT || PORT, () => console.log(`Server is running`)))
+    .then(() => app.listen(PORT, () => console.log(`Server is running`)))
     .catch((error) => console.log(error.message))
 
 mongoose.set('useFindAndModify', false)
